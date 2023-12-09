5b86f543c0271eabcd926d91dafa249db775d904
Gioele Lazzari <56556415+lazzarigioele@users.noreply.github.com>
2023-12-09 19:33:49 +0100

    clean outputs

--- src/gempipe/__init__.py
+++ src/gempipe/__init__.py
@@ -1,7 +1,41 @@
+import argparse
+
 
 from .moduleA import funcA
 from .moduleB import funcB
 
 
 
+def recon_command(args):
+    print(args)
+    
+    
+def derive_command(args):
+    print(args)
 
+
+
+if __name__ == "__main__":
+    
+    
+    parser = argparse.ArgumentParser(description='')
+    subparsers = parser.add_subparsers(title='', dest='subcommand', help='')
+
+    # Subparser for the 'recon' command
+    recon_parser = subparsers.add_parser('recon', help='')
+    recon_parser.add_argument('arg1', help='')
+    recon_parser.add_argument('arg2', help='')
+
+    # Subparser for the 'derive' command
+    derive_parser = subparsers.add_parser('derive', help='')
+    derive_parser.add_argument('arg1', help='')
+    derive_parser.add_argument('arg2', help='')
+
+    
+    args = parser.parse_args()
+    if args.subcommand == 'recon':
+        recon_command(args)
+    elif args.subcommand == 'derive':
+        derive_command(args)
+    else:
+        print("Invalid subcommand. Use 'recon' or 'derive'.")
